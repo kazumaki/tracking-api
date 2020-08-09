014201c4fc118c0dd0fff04baf454631e042be5c
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-08-09 14:57:32 -0300

    Add dependecy between references

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :measurements
+  has_many :measurements, dependent: :destroy
   validates_presence_of %i[name email password_confirmation password_digest]
   validates_uniqueness_of :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many(:measurements) }
+  it { should have_many(:measurements).dependent(:destroy) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
